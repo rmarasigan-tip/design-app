4adc295a3e0d1d4bc930705c37c7c70e41e39a49
rmarasigan-tip <qrcmarasigan@tip.edu.ph>
2021-11-18 17:56:02 +0000

    Added Login Files

---
+++ login.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+mkdir tempdir
+mkdir tempdir/templates
+mkdir tempdir/static
+
+cp login.py tempdir/.
+cp -r templates/* tempdir/templates/.
+cp -r static/* tempdir/static/.
+
+echo "FROM python" >> tempdir/Dockerfile
+echo "RUN pip install flask" >> tempdir/Dockerfile
+echo "COPY  ./static /home/myapp/static/" >> tempdir/Dockerfile
+echo "COPY  ./templates /home/myapp/templates/" >> tempdir/Dockerfile
+echo "COPY  login.py /home/myapp/" >> tempdir/Dockerfile
+echo "EXPOSE 8080" >> tempdir/Dockerfile
+echo "CMD python3 /home/myapp/login.py" >> tempdir/Dockerfile
+
+cd tempdir
+docker build -t loginapp .
+docker run -t -d -p 5050:5050 --name loginrunning designapp
+docker ps -a
+
